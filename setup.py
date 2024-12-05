eedf06d008de1731eaec95935b419a531cb09a33
Grzegorz Latosinski <glatosinski@antmicro.com>
2024-12-05 19:59:57 +0100

    [#69865] setup.py: Updated version of the sphinx-immaterial
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools_setup(
         'myst-parser',
         'sphinx',
         'sphinxcontrib-mermaid',
-        'sphinx-immaterial @ https://github.com/antmicro/sphinx-immaterial/releases/download/tip/sphinx_immaterial-0.0.post1.tip-py3-none-any.whl',  # noqa: E501
+        'sphinx-immaterial @ https://github.com/antmicro/sphinx-immaterial/releases/download/tip/sphinx_immaterial-0.0.post1-py3-none-any.whl',  # noqa: E501
         'sphinx_tabs',
     ],
     classifiers=[],
